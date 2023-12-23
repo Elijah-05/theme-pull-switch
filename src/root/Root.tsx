73a73d8861dde23bcfdcb3651c7754f01c2993fc
Elijah-05 <ellayab804@gmail.com>
2023-12-24 01:20:12 +0300

    switch pull animation implemented

--- src/root/Root.tsx
+++ src/root/Root.tsx
@@ -5,8 +5,9 @@ import { theme } from "../store/store";
 const Root = () => {
   const [getTheme, setTheme] = useAtom(theme);
   const [switchHovered, setSwitchHovered] = useState(false);
-  const isDark = getTheme === 'dark' ? true : false
-  const switchColor = getTheme === 'light' ? 'bg-white' : 'bg-primary' 
+  const [isSwitchClicked, setIsSwitchClicked] = useState(false);
+  const isDark = getTheme === "dark" ? true : false;
+  const switchColor = getTheme === "light" ? "bg-white" : "bg-primary";
 
   const handleSwitchHover = () => {
     setSwitchHovered(!switchHovered);
@@ -18,7 +19,10 @@ const Root = () => {
       localStorage.getItem("theme") == "light" ? "dark" : "light"
     );
     setTheme(getTheme == "light" ? "dark" : "light");
-    
+    setIsSwitchClicked(!isSwitchClicked);
+    setTimeout(() => {
+        setIsSwitchClicked(isSwitchClicked);
+    }, 200)
   };
 
   return (
@@ -32,21 +36,28 @@ const Root = () => {
           } duration-500`}
         />
         <div
-          className={`w-10 ${switchColor} h-16 -translate-x-1/2 rounded-full drop-shadow-[3px_14px_8px_rgba(0,0,0,0.2)] cursor-pointer -translate-y-6  hover:translate-y-0 duration-1000 hover:drop-shadow-[3px_10px_4px_rgba(0,0,0,0.3)] group`}
+          className={`w-10 ${switchColor} ${
+            isSwitchClicked ? "-translate-y-0 duration-500" : 'hover:-translate-y-4 duration-1000'
+          } h-16 -translate-x-1/2 rounded-full drop-shadow-[3px_14px_8px_rgba(0,0,0,0.2)] cursor-pointer -translate-y-8 hover:drop-shadow-[3px_10px_4px_rgba(0,0,0,0.3)] group`}
           onMouseEnter={handleSwitchHover}
           onMouseLeave={handleSwitchHover}
           onClick={handleToggleTheme}
         />
       </div>
       <div className={` mb-[30vh] flex flex-col text-white select-none`}>
-        <div className={`flex items-center gap-4 bg-gradient-to-t from-secondary to-primary ${isDark && 'text-transparent'} bg-clip-text drop-shadow-[0px_8px_10px_rgba(0,0,0,0.2)] duration-500`}>
-          <div className=""><span className="text-[242px] ">08:26</span>
-          <p className=" -mt-12 text-base text-white font-inter font-light tracking-wider text-center">
-          Monday, December 31, 2023
-        </p></div>
+        <div
+          className={`flex items-center gap-4 bg-gradient-to-t from-secondary to-primary ${
+            isDark && "text-transparent"
+          } bg-clip-text drop-shadow-[0px_8px_10px_rgba(0,0,0,0.2)] duration-500`}
+        >
+          <div className="">
+            <span className="text-[242px] ">08:26</span>
+            <p className=" -mt-12 text-base text-white font-inter font-light tracking-wider text-center">
+              Monday, December 31, 2023
+            </p>
+          </div>
           <span className="text-8xl mt-8">59</span>
         </div>
-        
       </div>
     </div>
   );
